ded8af3b413fc11619fc40055b957d7f826d54b4
Rachel Lombardi <rllombardi@ap20.uc.osg-htc.org>
2023-09-26 21:03:45 -0500

    add paths

--- fastqc.sh
+++ fastqc.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 # Executable name: fastqc.sh
 
+# Prepare software environment
+export PATH=/FastQC:/usr/lib/jvm/java-11-openjdk-amd64/bin:$PATH
+export LD_LIBRARY_PATH=/usr/lib/jvm/java-11-openjdk-amd64/lib/jli
+
 # Run FastQC to determine the quality of our raw .fastq sequencing data
 fastqc $1
